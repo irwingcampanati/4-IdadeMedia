5c0cd03c6487bd50ae44dbeef04866946826c120
irwingcampanati <irwingcampanati.cube@gmail.com>
2023-03-16 10:00:55 -0300

    att

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//  Elabore um algoritmo que calcule a idade média de 5 alunos.
+
+using System;
+
+namespace IdadeMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a idade de 5 alunos para encontrarmos a média de idade entre eles.");
+            Console.WriteLine("Idade do primeiro aluno");
+            int idade1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Idade do segundo aluno: ");
+            int idade2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Idade do terceiro aluno: ");
+            int idade3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Idade do quarto aluno: ");
+            int idade4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Idade do quinto aluno: ");
+            int idade5 = Convert.ToInt32(Console.ReadLine());
+
+            int media = ((idade1 + idade2 + idade3 + idade4 + idade5) / 5);
+            Console.WriteLine("A média de idade dos 5 alunos é igual a: " + media);
+
+        }
+    }
+}
